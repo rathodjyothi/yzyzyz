cb1bdd67ef9947a0b48b8fa36d1ce9c8b0ab403e
rathodjyothi <simarsahi7@gmail.com>
2022-04-26 00:14:16 +0530

    ec2.bat is changed

--- ec2.bat
+++ ec2.bat
@@ -1,4 +1,4 @@
-aws ec2 run-instances --image-id ami-046ba197a4a7f497e --count 1 --instance-type t2.micro --key-name jyothi --security-groups jyothi --region us-east-2
-hi hello everyone
+aws ec2 run-instances --image-id ami-046ba197a4a7f497e --count 1 --instance-type t2.micro --key-name jyothi --security-groups jyothi --region us-east
+this is fileeeeeeee
 
 
